f06cdd9b7a05b79d13b4bae10ada7a1da08a417c
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-10-22 18:17:17 +0200

    server.js is ready

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
-
+const shoppingListRoutes = require("./controller/ShoppingList");
 require("./db/connection");
+
 const port = 8000;
+
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use("/shoppingList", shoppingListRoutes); // Use shopping list routes
 
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:8000`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
